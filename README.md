cadd50d32e71c5b8f556825c969f1e4766784a45
ianctw16 <ian08160816@hotmail.com>
2017-10-30 20:08:31 +0800

    color and readme

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # ALGO_HW2
 ![Alt text](img.png)
+![Alt text](img2.png)
 ### 執行
 * 終端機 ```cd``` 到程式資料夾下  
 * 輸入  ```python3 algorithm_HW2.py```
+* 出現程式視窗
 
 ### 回答  
 * ```2c``` = Both two number are correct.
 * ```1c``` = Only one number is correct.
 * ```0c``` = Both two number are wrong.
+* 輸入完按按鈕 ```submit```
+
+### 重新
+* 重新開始按按鈕 ```RE```
--- algorithm_HW2.py
+++ algorithm_HW2.py
@@ -4,8 +4,8 @@ import tkinter as tk
 from tkinter import LEFT, BOTTOM, RIGHT, TOP
 
 window = tk.Tk()
-window.title('2*5 #C')
-window.configure(background='white')
+window.title('-------------2*5 #c Game-------------')
+window.configure(background='light yellow')
 # window.geometry('600x400')
 
 
@@ -14,10 +14,11 @@ def c_game(response):
     global guess_1, guess_2
     if(response == '2c'):
         # print('Correct. So the answer is', guess_1, guess_2)
-        var_system_ask_1.set('Correct. so the answer is')
+        var_system_ask_1.set('Correct. The number is')
         var_system_ask_2.set(guess_1)
         var_system_ask_3.set(guess_2)
         var_system_ask_4.set('')
+        print('Correct. The number is', guess_1, guess_2)
 
     elif(response == '1c'):
         # removeing node = [guess_1, guess_2]
@@ -43,6 +44,7 @@ def c_game(response):
         test = random.randrange(0, len(pool_2))
         guess_1 = pool_2[test][0]
         guess_2 = pool_2[test][1]
+        ask_ans()
 
     elif(response == '0c'):
         i = 0
@@ -57,8 +59,7 @@ def c_game(response):
         test = random.randrange(0, len(pool_2))
         guess_1 = pool_2[test][0]
         guess_2 = pool_2[test][1]
-
-    ask_ans()
+        ask_ans()
 
 
 def ask_ans():
@@ -68,6 +69,7 @@ def ask_ans():
     # print('is(', guess_1, guess_2, ')?')
     var_system_ask_2.set(guess_1)
     var_system_ask_3.set(guess_2)
+    print('asked:', guess_1, guess_2)
     # response = input(">>> Ans(2c, 1c, 0c): ")
     # c_game(response, guess_1, guess_2)
 
@@ -80,43 +82,67 @@ var_candidate_2.set('Candidates: ')
 var_candidate = tk.StringVar()
 l_candidate = tk.Label(f_top,
                        textvariable=var_candidate,
-                       bg='white')
+                       font=("Comic Sans MS", 12),
+                       bg='deep sky blue')
 l_candidate_2 = tk.Label(f_top,
                          textvariable=var_candidate_2,
-                         bg='white')
+                         font=("Comic Sans MS", 12),
+                         bg='deep sky blue')
 l_candidate_2.pack(side=LEFT)
 l_candidate.pack(side=LEFT)
 
 
 # System Ask Label
-f_top_2 = tk.Frame(window, bg='white')
+f_top_2 = tk.Frame(window, bg='light yellow')
 f_top_2.pack(side=TOP)
-l_title = tk.Label(f_top_2,
-                   textvariable='-------------2*5 #c Game-------------',
-                   bg='white')
-l_title.pack()
+
 var_system_ask_1 = tk.StringVar()
-var_system_ask_1.set('Is(')
+var_system_ask_1.set('Your number is(')
 var_system_ask_2 = tk.StringVar()
 var_system_ask_3 = tk.StringVar()
 var_system_ask_4 = tk.StringVar()
 var_system_ask_4.set(')?')
+
 l_system_ask_1 = tk.Label(f_top_2,
                           textvariable=var_system_ask_1,
-                          bg='white')
+                          font=("Comic Sans MS", 14),
+                          bg='light yellow')
 l_system_ask_2 = tk.Label(f_top_2,
                           textvariable=var_system_ask_2,
-                          bg='white')
+                          font=("Comic Sans MS", 18),
+                          bg='green yellow')
 l_system_ask_3 = tk.Label(f_top_2,
                           textvariable=var_system_ask_3,
-                          bg='white')
+                          font=("Comic Sans MS", 18),
+                          bg='green yellow')
 l_system_ask_4 = tk.Label(f_top_2,
                           textvariable=var_system_ask_4,
-                          bg='white')
+                          font=("Comic Sans MS", 14),
+                          bg='light yellow')
 l_system_ask_1.pack(side=LEFT)
 l_system_ask_2.pack(side=LEFT)
 l_system_ask_3.pack(side=LEFT)
 l_system_ask_4.pack(side=LEFT)
+
+
+def restart():
+    print('---------------restart---------------')
+    global pool_2, guess_1, guess_2
+    pool_2 = []
+    for i in range(0, 5):
+        for j in range(i+1, 5):
+            pool_2.append([i, j])
+
+    random.seed(time.time())
+    test = random.randrange(0, len(pool_2))
+    # print()
+    guess_1 = pool_2[test][0]
+    guess_2 = pool_2[test][1]
+    print('log: ')
+    var_system_ask_1.set('Your number is(')
+    var_system_ask_4.set(')?')
+    ask_ans()
+
 
 pool_2 = []
 for i in range(0, 5):
@@ -129,7 +155,7 @@ test = random.randrange(0, len(pool_2))
 # print()
 guess_1 = pool_2[test][0]
 guess_2 = pool_2[test][1]
-print('UI Start')
+print('log: ')
 ask_ans()
 '''
 # Answer Buttons
@@ -154,12 +180,29 @@ def e_submit():
     c_game(ans)
 
 
-f_bot = tk.Frame(window, bg='white')
+'''
+img = tk.PhotoImage(file='submit_button.png')
+img.width = 74
+img.height = 18
+'''
+f_bot = tk.Frame(window, bg='light yellow')
 f_bot.pack(side=BOTTOM)
-l_e = tk.Label(f_bot, text='A(2c,1c,0c):', bg='white')
+l_e = tk.Label(f_bot, text='A(2c,1c,0c):',
+               font=("Comic Sans MS", 14),
+               bg='light yellow')
 e = tk.Entry(f_bot)
-e_button = tk.Button(f_bot, text='submit', command=e_submit)
+e_button = tk.Button(f_bot,
+                     text='submit',
+                     bg='gold',
+                     font=("Comic Sans MS", 16),
+                     command=e_submit)
+restart_button = tk.Button(window,
+                           text='RE',
+                           bg='brown1',
+                           font=("Comic Sans MS", 20),
+                           command=restart)
 l_e.pack(side=LEFT)
-e.pack(side=RIGHT)
 e_button.pack(side=RIGHT)
+e.pack(side=RIGHT)
+restart_button.pack()
 window.mainloop()
